72c9b86f33c07901a68c5f640acc4cb93a97c6aa
apollo <1apollo.rowe@gmail.com>
2023-02-18 16:31:55 -0500

    escape game

---
+++ Homeworks/C23Escape/TODO.md
@@ -0,0 +1,16 @@
+Name: 
+
+TDD TODO/Task list
+
+**Build Tests**
+
+These are for implementing the EscapeGameBuilder's `makeGameManager()` method.
+
+| **#** | Test                                                            | Comments                         |
+|:-----:|:----------------------------------------------------------------|:---------------------------------|
+|   0   | Square Game Manager Coordinate Tests                            | used escape_square_coord_test.egc|
+|   1   | Square Game Builder Initialization Settings Tests               | max x & y = 2, 2 players         |
+|   2   | Square Game CLEAR tests                                         | placed at (2, 1)                 |
+|   3   | Hex Game Manager Coordinate Tests                               | used escape_hex_coord_test.egc   |
+|   4   | Hex Game Builder Initialization Settings Test                   | max x & y = 3, 3 players         |
+|   5   | Hex Game CLEAR tests                                            | placed at (0, 0)                 |
---
+++ Homeworks/C23Escape/test/escape/EscapeGameBuilderTest.java
@@ -0,0 +1,73 @@
+package escape;
+
+import escape.builder.EscapeGameBuilder;
+import escape.required.Coordinate;
+import escape.required.LocationType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EscapeGameBuilderTest {
+    private final EscapeGameBuilder square_coord_test_builder;
+    private final EscapeGameManager square_coord_test_manager;
+    private final EscapeGameBuilder hex_coord_test_builder;
+    private final EscapeGameManager hex_coord_test_manager;
+    EscapeGameBuilderTest() throws Exception {
+        square_coord_test_builder = new EscapeGameBuilder("test/escape_configs/escape_square_coord_test.egc");
+        square_coord_test_manager = square_coord_test_builder.makeGameManager();
+        hex_coord_test_builder = new EscapeGameBuilder("test/escape_configs/escape_hex_coord_test.egc");
+        hex_coord_test_manager = hex_coord_test_builder.makeGameManager();
+    }
+
+
+    @Test
+    public void testCoordinateSquare0(){
+        assertNotEquals(square_coord_test_manager.makeCoordinate(0, 1).getRow(), square_coord_test_manager.makeCoordinate(1, 0).getRow());
+        assertNotEquals(square_coord_test_manager.makeCoordinate(0, 1).getColumn(), square_coord_test_manager.makeCoordinate(1, 0).getColumn());
+        assertEquals(square_coord_test_manager.makeCoordinate(2,1).getRow(), square_coord_test_manager.makeCoordinate(2,1).getRow());
+        assertEquals(square_coord_test_manager.makeCoordinate(2,1).getColumn(), square_coord_test_manager.makeCoordinate(2,1).getColumn());
+
+        assertNull(square_coord_test_manager.makeCoordinate(-1,-1));
+        assertNull(square_coord_test_manager.makeCoordinate(3,3));
+    }
+    @Test
+    public void testCoordinateSquare1(){
+        assertEquals(square_coord_test_builder.getGameInitializer().getCoordinateType(), Coordinate.CoordinateType.SQUARE);
+        assertEquals(square_coord_test_builder.getGameInitializer().getxMax(),2);
+        assertEquals(square_coord_test_builder.getGameInitializer().getyMax(),2);
+        assertEquals(square_coord_test_builder.getGameInitializer().getPlayers().length, 2);
+    }
+    @Test
+    public void testCoordinateSquare2(){
+        assertEquals(square_coord_test_builder.getGameInitializer().getLocationInitializers()[0].locationType, LocationType.CLEAR);
+        assertEquals(square_coord_test_builder.getGameInitializer().getLocationInitializers()[0].x,2);
+        assertEquals(square_coord_test_builder.getGameInitializer().getLocationInitializers()[0].y,1);
+    }
+
+    @Test
+    public void testCoordinateHex0(){
+        assertNotEquals(hex_coord_test_manager.makeCoordinate(1,-1).getRow(), hex_coord_test_manager.makeCoordinate(-1,1).getRow());
+        assertNotEquals(hex_coord_test_manager.makeCoordinate(1,-1).getColumn(), hex_coord_test_manager.makeCoordinate(-1,1).getColumn());
+        assertEquals(hex_coord_test_manager.makeCoordinate(1,1).getRow(), hex_coord_test_manager.makeCoordinate(1,1).getRow());
+        assertEquals(hex_coord_test_manager.makeCoordinate(1,1).getColumn(), hex_coord_test_manager.makeCoordinate(1,1).getColumn());
+
+        assertNull(hex_coord_test_manager.makeCoordinate(-4,-4));
+        assertNull(hex_coord_test_manager.makeCoordinate(4,4));
+    }
+    @Test
+    public void testCoordinateHex1(){
+        assertEquals(hex_coord_test_builder.getGameInitializer().getCoordinateType(), Coordinate.CoordinateType.HEX);
+        assertEquals(hex_coord_test_builder.getGameInitializer().getxMax(),3);
+        assertEquals(hex_coord_test_builder.getGameInitializer().getyMax(),3);
+        assertEquals(hex_coord_test_builder.getGameInitializer().getPlayers().length,3);
+    }
+    @Test
+    public void testCoordinateHex2(){
+        assertEquals(hex_coord_test_builder.getGameInitializer().getLocationInitializers()[0].locationType, LocationType.CLEAR);
+        assertEquals(hex_coord_test_builder.getGameInitializer().getLocationInitializers()[0].x,0);
+        assertEquals(hex_coord_test_builder.getGameInitializer().getLocationInitializers()[0].y,0);
+    }
+}
